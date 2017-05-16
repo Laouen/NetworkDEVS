18aaaff9ed3f68d09941ed056671751a5553008e
Laouen <laouen.belloli@gmail.com>
2017-05-16 00:18:27 -0300

    working in the doxygen documentation

--- structures/sw.h
+++ structures/sw.h
@@ -8,12 +8,41 @@
 
 #include "mac.h"
 
+/**
+ * @namespace sw
+ * 
+ * This namespace is used to declare all the switches related datatypes.
+ */
 namespace sw {
+
+  /**
+   * 
+   * @author Laouen Louan Mayal Belloli
+   * @date 14 May 2017
+   * 
+   * @struct sw::Forwarding_entry
+   * 
+   * @brief This structures declares an entry of the Datagram protocol forwading table.
+   * @details The switches uses this datatype to fill their forwarding tables in order
+   * to determine through what interfaces forward a link::Frame.
+   * 
+   */
   struct Forwarding_entry {
     MAC mac;
     ushort interface;
 
+    /**
+     * @brief Default constructor
+     * @details Construct an empty instance.
+     */
     Forwarding_entry() {}
+
+    /**
+     * @brief Copy constructor
+     * @details Construct a copy of the instance passed as parameter in the new instance.
+     * 
+     * @param other The sw::Forwarding_entry to copy in the new instance.
+     */
     Forwarding_entry(const Forwarding_entry& other) {
       mac = other.mac;
       interface = other.interface;
--- structures/swp.h
+++ structures/swp.h
@@ -14,22 +14,57 @@
 #define FLAG_HAS_DATA 0x2 // TODO: check what number should go here;
 #define SWP_SEND_TIMEOUT 900 // TODO: check what number should go here, currently i put 900 seg = 15 min
 
+/**
+ * @namespace swp
+ * 
+ * This namespace is used to declare al the datatypes related to the SWP protocol
+ */
 namespace swp {
 
   typedef u_char Seqno;
 
+  /**
+   * 
+   * @author Laouen Louan Mayal Belloli 
+   * @date 14 May 2017
+   * 
+   * @struct swp::Hdr swp.h
+   *  
+   * @brief This structs declares a Hdr as explained in the book: Computer Networks A system approach - 2011.
+   * @details A Hdr has the next fields:
+   * Seqno SeqNum: sequence number of this frame
+   * Seqno AckNum: ack of received frame
+   * u_char Flags: up to 8 bits worth of flags
+   */
   struct Hdr {
 
+    /**
+     * @brief Default constructor
+     * @details Construct a reseted new instance.
+     */
     Hdr() {
       SeqNum = 0x0;
       AckNum = 0x0;
       Flags = 0x0; 
     }
-    Seqno SeqNum; // sequence number of this frame
-    Seqno AckNum; // ack of received frame
-    u_char Flags; // up to 8 bits worth of flags
+    Seqno SeqNum;
+    Seqno AckNum;
+    u_char Flags;
   };
 
+  /**
+   * 
+   * @author Laouen Louan Mayal Belloli
+   * @date 14 May 2017
+   * 
+   * @struct swp::sendQ_slot swp.h
+   * 
+   * @brief This strict declares a sendQ_slot as explained in the book: Computer Networks A system approach - 2011.
+   * @details This implementation differs from the one of the book because this 
+   * was changed to work with DEVS and PowerDEVS.
+   * 
+   * @tparam MSG the datatype of the send messages.
+   */
   template <typename MSG>
   struct sendQ_slot {
     double timeout; 
@@ -37,12 +72,39 @@ namespace swp {
     Seqno SeqNum;
   };
 
+  /**
+   * 
+   * @author Laouen Louan Mayal Belloli
+   * @date 14 May 2017
+   * 
+   * @struct swp::recQ_slot swp.h
+   * 
+   * @brief This strict declares a sendQ_slot as explained in the book: Computer Networks A system approach - 2011.
+   * @details This implementation differs from the one of the book because this 
+   * was changed to work with DEVS and PowerDEVS.
+   * 
+   * @tparam MSG the datatype of the recived messages.
+   * 
+   */
   template <typename MSG>
   struct recvQ_slot {
     Seqno SeqNum;
     MSG msg;
   };
   
+  /**
+   * 
+   * @author Laouen Louan Mayal Belloli
+   * @date 14 May 2017
+   * 
+   * @struct swp::state swp.h
+   * 
+   * @brief This strict declares a swp::state as explained in the book: Computer Networks A system approach - 2011.
+   * @details This implementation differs from the one of the book because this 
+   * was changed to work with DEVS and PowerDEVS.
+   * 
+   * @tparam MSG The datatype of the send/recived messages.
+   */
   template <typename MSG>
   struct State {
     // queued frames waiting to be send
@@ -57,6 +119,10 @@ namespace swp {
     Seqno LFR; // Last Frame Received
     std::list<recvQ_slot<MSG>> recvQ;
 
+    /**
+     * @brief Default constructor.
+     * @details Construct a restarted instance.
+     */
     State() {
       std::queue<link::Frame> empty;
       framesToSend.swap(empty);
@@ -67,6 +133,12 @@ namespace swp {
       LFR = 0;
     }
 
+    /**
+     * @brief Copy constructor
+     * @details Create a copy of the instance passed as parameter in the new instace.
+     * 
+     * @param other The swp::State to copy in the new instance.
+     */
     State(const State& other) {
       framesToSend = other.framesToSend;
       LAR = other.LAR;
@@ -76,18 +148,32 @@ namespace swp {
       recvQ = other.recvQ;
     }
 
+    /**
+     * @brief Checks whether no more MSGs can be send until a new Ack arrives 
+     * @return True if no more MSGs can be send, False otherwise.
+     */
     bool sendWindowsIsFull() const {
       return sendQ.size() >= SWS;
     }
 
+    /**
+     * @brief Checks whether there is MSGs waiting to be send.
+     * @return True if there is MSGs to send, False otherwise.
+     */
     bool thereIsFrameToSend() const {
       return !framesToSend.empty() && !this->sendWindowsIsFull();
     }
 
+    /**
+     * @brief Sends a link::Frame
+     */
     void sendFrame(link::Frame frame) {
       framesToSend.push(frame);
     }
 
+    /**
+     * @brief Resets the instance values to start from the begining.
+     */
     void reset() {
       std::queue<link::Frame> empty;
       framesToSend.swap(empty);
