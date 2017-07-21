44019f778c9b1efa49bcc1c5fc1e0603802cc776
Laouen <laouen.belloli@gmail.com>
2017-07-21 13:41:44 -0300

    finished Mati's correxion with new outputs

--- utils/logger_separator.py
+++ utils/logger_separator.py
@@ -3,14 +3,14 @@ import sys
 import gflags  # sudo pip install python-gflags
 
 gflags.DEFINE_string('log', None,
-					 'The path of the pdev.log file',
-					 short_name='l')
+                     'The path of the pdev.log file',
+                     short_name='l')
 
 gflags.MarkFlagAsRequired('log')
 
 gflags.DEFINE_string('destination_folder', None,
-					 'The path of the destination folder where to save results',
-					 short_name='f')
+                     'The path of the destination folder where to save results',
+                     short_name='f')
 
 gflags.MarkFlagAsRequired('log')
 gflags.MarkFlagAsRequired('destination_folder')
@@ -19,43 +19,43 @@ FLAGS = gflags.FLAGS
 
 
 try:
-	argv = FLAGS(sys.argv)
+    argv = FLAGS(sys.argv)
 except gflags.FlagsError as e:
-	print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
-	sys.exit(1)
+    print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
+    sys.exit(1)
 
 labels = {
-	'DNS': [],
-	'DDemux': [],
-	'Demux': [],
-	'Multiplexer': [],
-	'IPv4': [],
-	'Link': [],
-	'Switch': [],
-	'UDP': [],
-	'SWP': []
+    'DNS': [],
+    'DDemux': [],
+    'Demux': [],
+    'Multiplexer': [],
+    'IPv4': [],
+    'Link': [],
+    'Switch': [],
+    'UDP': [],
+    'SWP': []
 }
 
 last_label = ''
 with open(FLAGS.log) as f:
-	content = f.readlines()
-	for line in content:
+    content = f.readlines()
+    for line in content:
 
-		if 'Simulation Initialized' in line:
-			for label, logs in labels.iteritems():
-				logs.append(line)
-			continue	
+        if 'Simulation Initialized' in line or 'Simulation Ended' in line:
+            for label, logs in labels.iteritems():
+                logs.append(line)
+            continue
 
-		is_label = False
-		for label, logs in labels.iteritems():
-			if label in line:
-				last_label = label
-				is_label = True
-				logs.append(line)
+        is_label = False
+        for label, logs in labels.iteritems():
+            if ('[' + label) in line:
+                last_label = label
+                is_label = True
+                logs.append(line)
 
-		if not is_label:
-			labels[last_label].append(line)
+        if not is_label:
+            labels[last_label].append(line)
 
 for label, logs in labels.iteritems():
-	with open(FLAGS.destination_folder + "/" + label + ".log", 'w') as file:
-		file.writelines(logs)
+    with open(FLAGS.destination_folder + "/" + label + ".log", 'w') as file:
+        file.writelines(logs)
